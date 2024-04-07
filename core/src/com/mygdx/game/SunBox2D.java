97e82d07422f6b493a02f20e8b74893148407373
teacher <tibedox@mail.ru>
2024-04-07 13:53:33 +0600

    draw Beton

--- core/src/com/mygdx/game/SunBox2D.java
+++ core/src/com/mygdx/game/SunBox2D.java
@@ -11,19 +11,23 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SunBox2D extends ApplicationAdapter {
+	// глобальные константы
 	public static final float WORLD_WIDTH = 16, WORLD_HEIGHT = 9;
 
+	// системные объекты
 	SpriteBatch batch;
 	OrthographicCamera camera;
 	World world;
 	Box2DDebugRenderer debugRenderer;
 
-	Texture img;
+	// ресурсы
+	Texture imgBeton;
 
+	// наши объекты и переменные
 	StaticBody floor;
 	StaticBody wallLeft, wallRight;
 
-	DynamicBody[] ball = new DynamicBody[25];
+	DynamicBody[] balls = new DynamicBody[25];
 	
 	@Override
 	public void create () {
@@ -33,15 +37,14 @@ public class SunBox2D extends ApplicationAdapter {
 		world = new World(new Vector2(0, -9.8f), true);
 		debugRenderer = new Box2DDebugRenderer();
 
-		img = new Texture("badlogic.jpg");
+		imgBeton = new Texture("beton.png");
 
-		floor = new StaticBody(world, WORLD_WIDTH/2, 0.5f, WORLD_WIDTH, 1);
-		wallLeft = new StaticBody(world, 0.5f, WORLD_HEIGHT/2, 1, WORLD_HEIGHT);
-		wallRight = new StaticBody(world, WORLD_WIDTH-0.5f, WORLD_HEIGHT/2, 1, WORLD_HEIGHT);
-		for (int i = 0; i < ball.length; i++) {
-			ball[i] = new DynamicBody(world, WORLD_WIDTH/2+MathUtils.random(-0.01f, 0.01f), WORLD_HEIGHT+i, 0.4f);
+		floor = new StaticBody(world, 8, 0.5f, 16, 1);
+		wallLeft = new StaticBody(world, 0.5f, 5, 1, 8);
+		wallRight = new StaticBody(world, 15.5f, 5, 1, 8);
+		for (int i = 0; i < balls.length; i++) {
+			balls[i] = new DynamicBody(world, WORLD_WIDTH/2+MathUtils.random(-0.01f, 0.01f), WORLD_HEIGHT+i, 0.4f);
 		}
-
 	}
 
 	@Override
@@ -51,17 +54,19 @@ public class SunBox2D extends ApplicationAdapter {
 		debugRenderer.render(world, camera.combined);
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		/*for (int i = 0; i < ball.length; i++) {
-			batch.draw(img, ball[i].getX(), ball[i].getY(), ball[i].getWidth(), ball[i].getHeight());
+		/*for (int i = 0; i < balls.length; i++) {
+			batch.draw(img, balls[i].getX(), balls[i].getY(), balls[i].getWidth(), balls[i].getHeight());
 		}*/
-		//batch.draw(img, floor.getX(), floor.getY(), floor.getWidth(), floor.getHeight());
+		batch.draw(imgBeton, floor.getX(), floor.getY(), floor.getWidth(), floor.getHeight());
+		batch.draw(imgBeton, wallLeft.getX(), wallLeft.getY(), wallLeft.getWidth(), wallLeft.getHeight());
+		batch.draw(imgBeton, wallRight.getX(), wallRight.getY(), wallRight.getWidth(), wallRight.getHeight());
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		imgBeton.dispose();
 		world.dispose();
 		debugRenderer.dispose();
 	}
